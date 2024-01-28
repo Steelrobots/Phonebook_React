3d8c1d4e6ac1bcc45559bd0ec6f00a906628c4c7
Andri Priyadi <dripriyadi@gmail.com>
2024-01-28 14:36:44 +0700

    scroll done but not tested

--- src/components/PhoneList.js
+++ src/components/PhoneList.js
@@ -1,12 +1,37 @@
-import { useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { loadPhonebooks } from "../action/action";
+import { loadPage, loadPhonebooks } from "../action/action";
 import PhoneItem from "./PhoneItem";
 
 export default function PhoneList({ keyword, sort }) {
     const dispatch = useDispatch()
-    const { phonebooks} = useSelector(state => state.contacts)
+    const { phonebooks, page, pages } = useSelector(state => state.contacts)
+    const [isLoading, setIsLoading] = useState(false)
 
+    const handleScroll = useCallback(() => {
+        if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight && !isLoading) {
+            try {
+                if (page < pages) {
+                    setIsLoading(true)
+                    const newPage = page + 1
+                    dispatch(loadPage({ page: newPage, keyword, sort }))
+                }
+                else setIsLoading(false)
+            } catch (error) {
+                console.log(error)
+            } finally {
+                setIsLoading(false)
+            }
+        }
+
+    })
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll)
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, [dispatch, pages, page, keyword, sort])
 
     useEffect(() => {
         const readData = async () => {
